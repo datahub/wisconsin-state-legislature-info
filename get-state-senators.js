97e293a35e3803e03c9e120e4203616ee57de85d
Michael Andre <mtandre@users.noreply.github.com>
2017-02-03 12:08:31 -0600

    fix for phoneTwo and agnostic naming

--- get-state-senators.js
+++ get-state-senators.js
@@ -1,21 +1,21 @@
-var senInfo = [];
-$("[id^=district]").each(function(index,senator){
-	var image = $(senator).find('.picture').attr('src');
-	var website = $($(senator).find('a')[0]).attr('href');
-	var name = $($(senator).find('.info')[0]).find('strong a').text().split(',');
+var legislatureInfo = [];
+$("[id^=district]").each(function(index, person){
+	var image = $(person).find('.picture').attr('src');
+	var website = $($(person).find('a')[0]).attr('href');
+	var name = $($(person).find('.info')[0]).find('strong a').text().split(',');
 	var firstName = name[1];
 	var lastName = name[0];
-	var party = $($($(senator).find('.info')[0]).find('small')[0]).text();
-	var title = $($($(senator).find('.info')[0]).find('span span')[0]).text();
-	var district = $($($(senator).find('.info')[0]).find('span small')[1]).text();
-	var capitolAddress = $(senator).find('.office').text().replace(/\r?\n|\r/g,'').trim();
-	var phoneNumbers = $(senator).find('.telephone').text().replace(/\r?\n|\r/g,'').replace(/Telephone\:\s+/,'').trim().split('(');
-	var phoneOne = phoneNumbers[1].replace('\)|\s|-','')
-	var phoneTwo = (phoneNumbers.length > 2) ? phoneNumbers[2].replace('\)|\s|-','') : '';
-	var email = $(senator).find('.email a').text();
-	var votingAddress = $(senator).find('.voting_address').text().replace(/\r?\n|\r/g,'').replace(/Voting Address\:\s+/,'').trim();
-	var staff = $(senator).find('.staff a').text().split('.gov').join('.gov | ').replace(/ \| $/,'');
-	senInfo.push({
+	var party = $($($(person).find('.info')[0]).find('small')[0]).text();
+	var title = $($($(person).find('.info')[0]).find('span span')[0]).text();
+	var district = $($($(person).find('.info')[0]).find('span small')[1]).text();
+	var capitolAddress = $(person).find('.office').text().replace(/\r?\n|\r/g,'').trim();
+	var phoneNumbers = $(person).find('.telephone').text().replace(/\r?\n|\r/g,'').replace(/Telephone\:\s+/,'').trim().split('(');
+	var phoneOne = "("+phoneNumbers[1];
+	var phoneTwo = (phoneNumbers.length > 2) ? "("+phoneNumbers[2] : '';
+	var email = $(person).find('.email a').text();
+	var votingAddress = $(person).find('.voting_address').text().replace(/\r?\n|\r/g,'').replace(/Voting Address\:\s+/,'').trim();
+	var staff = $(person).find('.staff a').text().split('.gov').join('.gov | ').replace(/ \| $/,'');
+	legislatureInfo.push({
 		firstName: firstName,
 		lastName: lastName,
 		image: image,
@@ -30,4 +30,4 @@ $("[id^=district]").each(function(index,senator){
 		staff: staff
 	});
 });
-copy(senInfo);
+copy(legislatureInfo);
